9295f6ca6ad3bbbb20e7b2650a3a905cd9d508d0
anilwagle808 <anil.wagle808@gmail.com>
2025-09-10 08:50:23 +0545

    Adding loading previous one

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-// server.js
 import fs from 'node:fs/promises'
 import express from 'express'
 
+// Constants
 const isProduction = process.env.NODE_ENV === 'production'
+const port = process.env.PORT || 5173
 const base = process.env.BASE || '/'
 
+// Cached production assets
 const templateHtml = isProduction
   ? await fs.readFile('./dist/client/index.html', 'utf-8')
   : ''
 
+// Create http server
 const app = express()
 
+// Add Vite or respective production middlewares
+/** @type {import('vite').ViteDevServer | undefined} */
 let vite
 if (!isProduction) {
   const { createServer } = await import('vite')
@@ -27,13 +32,17 @@ if (!isProduction) {
   app.use(base, sirv('./dist/client', { extensions: [] }))
 }
 
-app.use(/.*/, async (req, res) => {
+// Serve HTML
+app.use('*all', async (req, res) => {
   try {
     const url = req.originalUrl.replace(base, '')
 
+    /** @type {string} */
     let template
+    /** @type {import('./src/entry-server.ts').render} */
     let render
     if (!isProduction) {
+      // Always read fresh template in development
       template = await fs.readFile('./index.html', 'utf-8')
       template = await vite.transformIndexHtml(url, template)
       render = (await vite.ssrLoadModule('/src/entry-server.tsx')).render
@@ -51,12 +60,12 @@ app.use(/.*/, async (req, res) => {
     res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
   } catch (e) {
     vite?.ssrFixStacktrace(e)
-    console.error(e.stack)
+    console.log(e.stack)
     res.status(500).end(e.stack)
   }
 })
 
-// 👇 DO NOT call app.listen() on Vercel
-// app.listen(port, () => console.log(`Server started: http://localhost:${port}`))
-
-export default app
+// Start http server
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`)
+})
